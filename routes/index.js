b700a55ed0212f7425143cb938264903d2481461
ymann <ymann@wharton.upenn.edu>
2017-09-17 00:39:16 -0400

    Post + Get func
    Added post and get functionality for lists and cards. Added debugging tools - page to add and see current db values.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 var mongo = require('mongodb').MongoClient;
 var assert = require('assert');
 
-var url = 'mongodb://localhost:27017/organizations';
+var url = 'mongodb://localhost:27017/trello';
+
+function addAndUpdateOrders(list, newIndex, db) {
+    if (db.mycollection.find({order: { "$in": newIndex}}).count() > 0) { // Check if the order spot is already taken
+        db.collection('lists').updateMany({"order": {$gte: newIndex}}, {$inc: {order: 1}}, function (err, result) { // If yes, update then insert
+            assert.equal(null, err);
+            console.log('Index updated');
+            db.collection('lists').insertOne(list, function (err, res) { // If no just insert
+                assert.equal(null, err);
+                console.log('List Inserted');
+                db.close();
+            });
+        });
+    } else {
+        db.collection('lists').insertOne(list, function (err, res) {
+        assert.equal(null, err);
+        console.log('List Inserted');
+        db.close();
+        });
+    }
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,56 +31,101 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.get('/organizations', function(req, res, next) {
-    var resultArray = [];
+router.post('/list', function(req, res, next) {
     mongo.connect(url, function(err, db) {
-        assert.equal(null, err);
-        var cursor = db.collection('clubs').find();
-        cursor.forEach(function(doc, err) {
-            assert.equal(null, err);
-            resultArray.push(doc);
-        }, function() {
-            db.close();
-            res.render('clubs', {clubs: resultArray});
-        });
-    });
-});
-
-router.get('/organizations/:id', function(req, res, next) {
-    var returnDoc = null;
-    mongo.connect(url, function(err, db) {
-        assert.equal(null, err);
-        var cursor = db.collection('clubs').find();
-        cursor.forEach(function(doc, err) {
-            assert.equal(null, err);
-            if(doc._id == req.params.id) {
-                returnDoc = doc;
+        var order;
+        if (req.body.order == null) {
+            order = db.collection('lists').count();
+        } else {
+            if (parseInt(req.body.order) >= db.collection('lists').count()) {
+                order = db.collection('lists').count(); // Cut off order at max
+            } else {
+                order = parseInt(req.body.order, 10);
             }
-        }, function() {
-            db.close();
-            res.render('club-page', {club: returnDoc});
-        });
+        }
+        // Create list to insert
+        var list = {
+            title: req.body.title,
+            id: req.body.id,
+            order: order,
+        };
+        addAndUpdateOrders(list, order, db)
     });
+    res.redirect('/');
 });
 
-router.post('/insert', function(req, res, next) {
-    var club = {
-        name: req.body.name,
+router.post('/card', function(req, res, next) {
+    var cart = {
+        title: req.body.title,
         description: req.body.description,
-        url: req.body.url,
-        tags: req.body.tags
+        listId: req.body.listId,
+        id: req.body.id,
     };
 
     mongo.connect(url, function(err, db) {
         assert.equal(null, err);
-        db.collection('clubs').insertOne(club, function(err, res) {
+        db.collection('cards').insertOne(cart, function(err, res) {
             assert.equal(null, err);
-            console.log('Item Inserted');
+            console.log('Card Inserted');
             db.close();
         });
     });
-
     res.redirect('/');
+});
+
+router.get('/card/:cardId', function(req, res, next) {
+    mongo.connect(url, function(err, db) {
+        var returnDoc;
+        assert.equal(null, err);
+        var cursor = db.collection('cards').find();
+        cursor.forEach(function(doc, err) {
+            assert.equal(null, err);
+            if(doc.id == req.params.cardId) {
+                returnDoc = doc;
+            }
+        }, function() {
+            db.close();
+            res.render('card', {card: returnDoc});
+        });
+    });
+});
+
+router.get('/list/:listId', function(req, res, next) {
+    mongo.connect(url, function(err, db) {
+        var returnDoc;
+        assert.equal(null, err);
+        var cursor = db.collection('lists').find();
+        cursor.forEach(function(doc, err) {
+            assert.equal(null, err);
+            if(doc.id == req.params.listId) {
+                returnDoc = doc;
+            }
+        }, function() {
+            db.close();
+            res.render('list', {list: returnDoc});
+        });
+    });
+});
+
+router.get('/get-data', function(req, res, next) {
+    var listsArray = [];
+    var cardsArray = [];
+    mongo.connect(url, function(err, db) {
+        assert.equal(null, err);
+        var cursor = db.collection('lists').find();
+        var cursor2 = db.collection('cards').find();
+        cursor2.forEach(function(doc, err) {
+            assert.equal(null, err);
+            cardsArray.push(doc);
+        });
+        cursor.forEach(function(doc, err) {
+            assert.equal(null, err);
+            listsArray.push(doc);
+        }, function() {
+            db.close();
+            res.render('index', {lists: listsArray, carts: cardsArray});
+        });
+    });
 });
 
 module.exports = router;
